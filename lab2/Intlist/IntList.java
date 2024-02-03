96171402992ae4694a7424bdf65da57284bfea20
Shaomin <xushaominchina@163.com>
2024-02-03 16:06:39 -0500

    update lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,8 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null)
+            return null;
         IntList ptr = A;
         while (ptr.rest != null){
             ptr = ptr.rest;
@@ -96,6 +98,8 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null)
+            return null;
         IntList res = new IntList(A.first, null);
         IntList ptr = res;
         while (A.rest != null){
